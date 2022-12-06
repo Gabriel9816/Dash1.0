869afc5bb6f39da53e06e47a3eb1a3a7c88a473a
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-12-05 22:43:48 -0400

    primeiro commit

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user',           component: UserComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'upgrade',        component: UpgradeComponent }
+];
